4d91477587e5eee7d8a5e16b73d3d466170c8a16
harshita-42 <harshitad1142@gmail.com>
2024-12-19 11:44:17 -0600

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,61 @@
+"""
+URL configuration for pyshop project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+from django.http import HttpResponse
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PyShop API Documentation",
+        default_version='v1',
+        description="API documentation for the PyShop application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="admin@pyshop.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # Generate OpenAPI schema
+    #path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
+    path('new/', lambda request: HttpResponse('New products'), name='new'),
+    #path('har/', lambda request: HttpResponse('Hi'), name='har'),
+    #path('index/', lambda request: HttpResponse('Index page'), name='index'),
+    #path('hari/', lambda request: JsonResponse({"message": "Hello, World!"}), name='hari'),
+
+
+    # Swagger URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
